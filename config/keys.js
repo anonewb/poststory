0ca47ce010b494a844dce4030e585d001e96adac
anonewb <anonewbe@gmail.com>
2018-06-21 16:58:39 +0530

    creating our google oauth credentials

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  monogoURI:'mongodb://ano:ano123@ds161710.mlab.com:61710/storybooks-34',
+  googleClientID: '718931043006-kea57t305ol7286gd7b9rh05u05im25k.apps.googleusercontent.com',
+  googleClientSecret: 'A1Eo4-bPUJOZF4m1bf4n-mjK'
+}
