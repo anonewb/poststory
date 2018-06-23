c7958130a9f622b638574d1fbd4b9302b9368dd8
anonewb <anonewbe@gmail.com>
2018-06-23 10:23:30 +0530

    deployment- pre production key vars

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  monogoURI:'mongodb://ano:ano123@ds161710.mlab.com:61710/storybooks-34',
-  googleClientID: '718931043006-kea57t305ol7286gd7b9rh05u05im25k.apps.googleusercontent.com',
-  googleClientSecret: 'A1Eo4-bPUJOZF4m1bf4n-mjK'
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
